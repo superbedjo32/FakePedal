c90b17a9199e47ca5f11d078e236c57c108cb7ab
unknown <alafair32@gmail.com>
2023-12-12 00:28:16 +0700

    [Bab 7] Handling Text Input

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './src/router';
 
